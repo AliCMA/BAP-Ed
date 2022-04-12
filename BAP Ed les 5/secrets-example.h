31f5f735cee8318c46a2428289ec0f726c378928
AliCMA <90691339+AliCMA@users.noreply.github.com>
2022-04-12 19:33:16 +0200

    BAP Ed les 5

---
+++ BAP Ed les 5/secrets-example.h
@@ -0,0 +1,9 @@
+#define WIFI_SSID "ChangeMe"
+#define WIFI_PASSWORD "ChangeMe"
+#define SERVER_HOST "data.softwaredeveloper.amsterdam"
+#define SERVER_PORT 443
+#define DEVICE_KEY "ChangeMe"
+#define DEVICE_SECRET "ChangeMe"
+
+
+// Define alles hiermee 
