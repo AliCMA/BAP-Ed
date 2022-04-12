f00e2fb83d2ceea085baad4da70b5a2d49356b2d
AliCMA <90691339+AliCMA@users.noreply.github.com>
2022-04-12 19:21:20 +0200

    BAP Ed les 5

---
+++ BAP Ed les 5/secrets.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID "Medialab"
+#define WIFI_PASSWORD "Mediacollege2"
+#define SERVER_HOST "data.softwaredeveloper.amsterdam"
+#define SERVER_PORT 443
+#define DEVICE_KEY "d8b80213" // gebruik jouw eigen key
+#define DEVICE_SECRET "CaPA8AgE" // gebruik jouw eigen secret
